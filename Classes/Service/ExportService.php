76f8e8991ee4e3213b1ca7b4c51771acd95eafb8
Robert Baruck <robert.baruck@sandstorm.de>
2023-08-25 13:00:44 +0200

    TASK: Prevent null exception in Export->writeProperty() when propertyValue is null

--- Classes/Service/ExportService.php
+++ Classes/Service/ExportService.php
@@ -369,7 +369,7 @@ class ExportService extends AbstractService
         $this->xmlWriter->endElement();
     }
 
-    protected function writeProperty(string $propertyName, string $propertyValue): void
+    protected function writeProperty(string $propertyName, ?string $propertyValue = ''): void
     {
         $this->xmlWriter->startElement($propertyName);
         $this->xmlWriter->writeAttribute('type', 'string');
